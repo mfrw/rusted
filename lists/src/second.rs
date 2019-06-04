a9c66e74dcd3c51bfd2aa0b9e8dae8aafd396040
Muhammad Falak R Wani <falakreyaz@gmail.com>
2019-06-04 22:43:22 +0530

    second: lifetime elision

--- lists/src/second.rs
+++ lists/src/second.rs
@@ -67,7 +67,7 @@ pub struct Iter<'a, T> {
 }
 
 impl<T> List<T> {
-    pub fn iter<'a>(&'a self) -> Iter<'a, T> {
+    pub fn iter(&self) -> Iter<T> {
         Iter {
             next: self.head.as_ref().map(|node| &**node),
         }
