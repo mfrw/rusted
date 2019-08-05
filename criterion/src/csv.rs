6af81f2bad363659d4d0af6b45f52840d5cd1643
Muhammad Falak R Wani <falakreyaz@gmail.com>
2019-08-05 07:15:51 +0530

    criterion: csv headers and better error handling

--- criterion/src/csv.rs
+++ criterion/src/csv.rs
@@ -1,4 +1,6 @@
+use std::error::Error;
 use std::io;
+use std::process;
 
 pub fn csv_writer() {
     //let dollar_films = vec![
@@ -6,17 +8,29 @@ pub fn csv_writer() {
     //("For a Few Dollars More", "El Indio", 1965),
     //("The Good, the Bad and the Ugly", "Tuco", 1966),
     //];
+    //let _path = "westerns.cvs";
 
-    let _path = "westerns.cvs";
     let mut writer = csv::Writer::from_writer(io::stdout());
     writer.write_record(&["a", "b", "c"]).expect("CSV Error");
 }
 
 pub fn csv_reader() {
+    if let Err(err) = run() {
+        println!("{}", err);
+        process::exit(1);
+    }
+}
+
+fn run() -> Result<(), Box<dyn Error>> {
     let path = "test.csv";
-    let mut rdr = csv::Reader::from_path(path).expect("Not Found");
+    let mut rdr = csv::Reader::from_path(path)?;
+    {
+        let headers = rdr.headers()?;
+        println!("{:?}", headers);
+    }
     for result in rdr.records() {
-        let record = result.expect("CSV error");
+        let record = result?;
         println!("{:?}", record);
     }
+    Ok(())
 }
