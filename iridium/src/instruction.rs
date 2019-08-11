34a9c3010915cac527e8f8a6c556c7450d7a6aa0
Muhammad Falak R Wani <falakreyaz@gmail.com>
2019-08-11 15:28:00 +0530

    iridium: add instruction

---
+++ iridium/src/instruction.rs
@@ -0,0 +1,33 @@
+#[derive(Debug, PartialEq)]
+pub enum Opcode {
+    HLT,
+    IGL,
+}
+
+#[derive(Debug, PartialEq)]
+pub struct Instruction {
+    opcode: Opcode,
+}
+
+impl Instruction {
+    pub fn new(opcode: Opcode) -> Self {
+        Instruction { opcode: opcode }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_create_hlt() {
+        let opcode = Opcode::HLT;
+        assert_eq!(opcode, Opcode::HLT);
+    }
+
+    #[test]
+    fn test_create_instruction() {
+        let instruction = Instruction::new(Opcode::HLT);
+        assert_eq!(instruction.opcode, Opcode::HLT);
+    }
+}
