a61847dd8c2dbc9a872964642d68254300f43d8f
Muhammad Falak R Wani <falakreyaz@gmail.com>
2019-08-12 19:19:43 +0530

    rcb: pbkdf2: basic impl

--- rcb/src/crypto/mod.rs
+++ rcb/src/crypto/mod.rs
@@ -1,4 +1,5 @@
 mod hmac;
+mod pbkdf2;
 mod sha256;
 
 pub fn run() {
@@ -8,5 +9,8 @@ pub fn run() {
 
     if let Err(err) = hmac::run() {
         println!("Error in HMAC: {}", err);
+    }
+    if let Err(err) = pbkdf2::run() {
+        println!("Error in PBKDF2: {}", err);
     }
 }
