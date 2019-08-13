398082095fd98313b6424ca6848d7471158aae19
Muhammad Falak R Wani <falakreyaz@gmail.com>
2019-08-13 21:49:49 +0530

    iridium: vms: add LOAD op

--- iridium/src/vm.rs
+++ iridium/src/vm.rs
@@ -15,31 +15,68 @@ impl VM {
             pc: 0,
         }
     }
+
+    /// Loops as long as instructions can be executed.
     pub fn run(&mut self) {
-        loop {
-            if self.pc == self.program.len() {
-                break;
-            }
-            match self.decode_opcode() {
-                Opcode::HLT => {
-                    println!("HLT encountered");
-                    return;
-                }
-
-                Opcode::LOAD => {
-                    let register = self.next_8_bits() as usize;
-                    let number = self.next_16_bits() as u16;
-                    self.registers[register] = number as i32;
-                    continue;
-                }
-
-                _ => {
-                    println!("Unrecognized opcode found! Terminating!");
-                    return;
-                }
-            }
+        let mut is_done = false;
+        while !is_done {
+            is_done = self.execute_instruction();
         }
     }
+
+    /// Executes one instruction. Meant to allow for more controlled execution of the VM
+    pub fn run_once(&mut self) -> bool {
+        self.execute_instruction()
+    }
+
+    fn execute_instruction(&mut self) -> bool {
+        if self.pc >= self.program.len() {
+            return true;
+        }
+        match self.decode_opcode() {
+            Opcode::LOAD => {
+                let register = self.next_8_bits() as usize;
+                let number = self.next_16_bits() as u32;
+                self.registers[register] = number as i32;
+            }
+            Opcode::HLT => {
+                println!("HLT encountered");
+                return true;
+            }
+            _ => {
+                println!("Unrecognized opcode found! Terminating!");
+                return true;
+            }
+        }
+        true
+    }
+
+    //pub fn runt(&mut self) {
+    //loop {
+    //if self.pc == self.program.len() {
+    //break;
+    //}
+
+    //match self.decode_opcode() {
+    //Opcode::HLT => {
+    //println!("HLT encountered");
+    //return;
+    //}
+
+    //Opcode::LOAD => {
+    //let register = self.next_8_bits() as usize;
+    //let number = self.next_16_bits() as u16;
+    //self.registers[register] = number as i32;
+    //continue;
+    //}
+
+    //_ => {
+    //println!("Unrecognized opcode found! Terminating!");
+    //return;
+    //}
+    //}
+    //}
+    //}
 
     fn decode_opcode(&mut self) -> Opcode {
         let opcode = Opcode::from(self.program[self.pc]);
@@ -64,6 +101,10 @@ impl VM {
 mod tests {
     use super::*;
 
+    fn get_test_vm() -> VM {
+        VM::new()
+    }
+
     #[test]
     fn test_create_vm() {
         let test_vm = VM::new();
@@ -86,5 +127,13 @@ mod tests {
         test_vm.program = test_bytes;
         test_vm.run();
         assert_eq!(test_vm.pc, 1);
+    }
+
+    #[test]
+    fn test_load_opcode() {
+        let mut test_vm = get_test_vm();
+        test_vm.program = vec![0, 0, 1, 244]; // Remember, this is how we represent 500 using two u8s in little endian format
+        test_vm.run();
+        assert_eq!(test_vm.registers[0], 500);
     }
 }
