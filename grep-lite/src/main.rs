18979df1e6389440840c170ccac5dc93505ab119
Muhammad Falak R Wani <falakreyaz@gmail.com>
2019-12-26 21:49:13 +0530

    grep-lite: model state using enums

--- grep-lite/src/main.rs
+++ grep-lite/src/main.rs
@@ -2,6 +2,13 @@
 struct File {
     name: String,
     data: Vec<u8>,
+    state: FileState,
+}
+
+#[derive(Debug, PartialEq)]
+enum FileState {
+    Open,
+    Closed,
 }
 
 fn main() {
@@ -10,7 +17,7 @@ fn main() {
 
     let mut buffer: Vec<u8> = vec![];
     open(&mut f1);
-    let f1_len = read(&f1, &mut buffer);
+    let f1_len = f1.read(&mut buffer).unwrap();
     close(&mut f1);
 
     let text = String::from_utf8_lossy(&buffer);
@@ -26,27 +33,34 @@ impl File {
         File {
             name: String::from(name),
             data: vec![],
+            state: FileState::Closed,
         }
     }
+
     fn new_with_data(name: &str, data: &Vec<u8>) -> Self {
         let mut f = File::new(name);
         f.data = data.clone();
         f
     }
+
+    fn read(self: &File, save_to: &mut Vec<u8>) -> Result<usize, String> {
+        if self.state != FileState::Open {
+            return Err(String::from("File must be open for reading"));
+        }
+        let mut tmp = self.data.clone();
+        let read_length = tmp.len();
+        save_to.reserve(read_length);
+        save_to.append(&mut tmp);
+        Ok(read_length)
+    }
 }
 
 fn open(f: &mut File) -> bool {
+    f.state = FileState::Open;
     true
 }
 
 fn close(f: &mut File) -> bool {
+    f.state = FileState::Closed;
     true
-}
-
-fn read(f: &File, save_to: &mut Vec<u8>) -> usize {
-    let mut tmp = f.data.clone();
-    let read_length = tmp.len();
-    save_to.reserve(read_length);
-    save_to.append(&mut tmp);
-    read_length
 }
