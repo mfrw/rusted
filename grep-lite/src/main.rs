df726a4e02cf5922c9f0a4359ecdce7a2cca1b56
Muhammad Falak R Wani <falakreyaz@gmail.com>
2019-12-26 21:14:11 +0530

    grep-lite: use struct to model a file

--- grep-lite/src/main.rs
+++ grep-lite/src/main.rs
@@ -1,6 +1,27 @@
-#![allow(unused_variables)]
+#[derive(Debug)]
+struct File {
+    name: String,
+    data: Vec<u8>,
+}
 
-type File = String;
+fn main() {
+    let mut f1 = File {
+        name: String::from("f1.txt"),
+        data: vec![114, 117, 115, 116, 33],
+    };
+
+    let mut buffer: Vec<u8> = vec![];
+    open(&mut f1);
+    let f1_len = read(&f1, &mut buffer);
+    close(&mut f1);
+
+    let text = String::from_utf8_lossy(&buffer);
+
+    let f_name = &f1.name;
+    println!("{:?}", f1);
+    println!("{} is {} bytes long", f_name, f1_len);
+    println!("{}", text)
+}
 
 fn open(f: &mut File) -> bool {
     true
@@ -10,14 +31,10 @@ fn close(f: &mut File) -> bool {
     true
 }
 
-#[allow(dead_code)]
-fn read(f: &mut File, save_to: &mut Vec<u8>) -> ! {
-    unimplemented!()
-}
-
-fn main() {
-    let mut f1 = File::from("f1.txt");
-    open(&mut f1);
-    // read(f1, vec![]);
-    close(&mut f1);
+fn read(f: &File, save_to: &mut Vec<u8>) -> usize {
+    let mut tmp = f.data.clone();
+    let read_length = tmp.len();
+    save_to.reserve(read_length);
+    save_to.append(&mut tmp);
+    read_length
 }
