3031c82e16da4123beabe69f4410b2b3b8261e2b
Muhammad Falak R Wani <falakreyaz@gmail.com>
2019-12-26 21:32:30 +0530

    grep-lite: use impl patter for new objects
    Signed-off-by: Muhammad Falak R Wani <falakreyaz@gmail.com>

--- grep-lite/src/main.rs
+++ grep-lite/src/main.rs
@@ -5,10 +5,8 @@ struct File {
 }
 
 fn main() {
-    let mut f1 = File {
-        name: String::from("f1.txt"),
-        data: vec![114, 117, 115, 116, 33],
-    };
+    let f1_data: Vec<u8> = vec![114, 117, 115, 116, 33];
+    let mut f1 = File::new_with_data("f1.txt", &f1_data);
 
     let mut buffer: Vec<u8> = vec![];
     open(&mut f1);
@@ -21,6 +19,20 @@ fn main() {
     println!("{:?}", f1);
     println!("{} is {} bytes long", f_name, f1_len);
     println!("{}", text)
+}
+
+impl File {
+    fn new(name: &str) -> Self {
+        File {
+            name: String::from(name),
+            data: vec![],
+        }
+    }
+    fn new_with_data(name: &str, data: &Vec<u8>) -> Self {
+        let mut f = File::new(name);
+        f.data = data.clone();
+        f
+    }
 }
 
 fn open(f: &mut File) -> bool {
