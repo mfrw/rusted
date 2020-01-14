78ff44cc3952e3a4c5bf603e1a09d12a8fa06125
Muhammad Falak R Wani <falakreyaz@gmail.com>
2020-01-15 00:05:15 +0530

    grep-lite: inital version with harcoded lines
    Signed-off-by: Muhammad Falak R Wani <falakreyaz@gmail.com>

--- grep-lite/src/main.rs
+++ grep-lite/src/main.rs
@@ -1,66 +1,23 @@
-#[derive(Debug)]
-struct File {
-    name: String,
-    data: Vec<u8>,
-    state: FileState,
-}
-
-#[derive(Debug, PartialEq)]
-enum FileState {
-    Open,
-    Closed,
-}
+use clap::{App, Arg};
+use regex::Regex;
 
 fn main() {
-    let f1_data: Vec<u8> = vec![114, 117, 115, 116, 33];
-    let mut f1 = File::new_with_data("f1.txt", &f1_data);
+    let args = App::new("grep-lite")
+        .version("0.1")
+        .about("searches for patterns")
+        .arg(Arg::with_name("pattern").takes_value(true).required(true))
+        .get_matches();
 
-    let mut buffer: Vec<u8> = vec![];
-    open(&mut f1);
-    let f1_len = f1.read(&mut buffer).unwrap();
-    close(&mut f1);
+    let pattern = args.value_of("pattern").unwrap();
+    let re = Regex::new(pattern).unwrap();
 
-    let text = String::from_utf8_lossy(&buffer);
+    let quote = "Every face, every shop, bedroom window, public-house, and dark square is a picture feverishly turned--in search of what?
+It is the same with books. What do we seek through millions of pages?";
 
-    let f_name = &f1.name;
-    println!("{:?}", f1);
-    println!("{} is {} bytes long", f_name, f1_len);
-    println!("{}", text)
-}
-
-impl File {
-    fn new(name: &str) -> Self {
-        File {
-            name: String::from(name),
-            data: vec![],
-            state: FileState::Closed,
+    for line in quote.lines() {
+        match re.find(line) {
+            Some(_) => println!("{}", line),
+            None => (),
         }
     }
-
-    fn new_with_data(name: &str, data: &Vec<u8>) -> Self {
-        let mut f = File::new(name);
-        f.data = data.clone();
-        f
-    }
-
-    fn read(self: &File, save_to: &mut Vec<u8>) -> Result<usize, String> {
-        if self.state != FileState::Open {
-            return Err(String::from("File must be open for reading"));
-        }
-        let mut tmp = self.data.clone();
-        let read_length = tmp.len();
-        save_to.reserve(read_length);
-        save_to.append(&mut tmp);
-        Ok(read_length)
-    }
-}
-
-fn open(f: &mut File) -> bool {
-    f.state = FileState::Open;
-    true
-}
-
-fn close(f: &mut File) -> bool {
-    f.state = FileState::Closed;
-    true
 }
