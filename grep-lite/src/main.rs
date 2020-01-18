6858b75560793b300201060656083847ee07e47f
Muhammad Falak R Wani <falakreyaz@gmail.com>
2020-01-18 18:45:36 +0530

    grep-lite: add support for stdin
    Signed-off-by: Muhammad Falak R Wani <falakreyaz@gmail.com>

--- grep-lite/src/main.rs
+++ grep-lite/src/main.rs
@@ -2,6 +2,7 @@ use clap::{App, Arg};
 use regex::Regex;
 
 use std::fs::File;
+use std::io;
 use std::io::prelude::*;
 use std::io::BufReader;
 
@@ -26,10 +27,19 @@ fn main() {
     let pattern = args.value_of("pattern").unwrap();
     let re = Regex::new(pattern).unwrap();
 
-    let input = args.value_of("input").unwrap();
-    let f = File::open(input).unwrap();
-    let reader = BufReader::new(f);
+    let input = args.value_of("input").unwrap_or("-");
+    if input == "-" {
+        let stdin = io::stdin();
+        let reader = stdin.lock();
+        process_lines(reader, re);
+    } else {
+        let f = File::open(input).unwrap();
+        let reader = BufReader::new(f);
+        process_lines(reader, re);
+    }
+}
 
+fn process_lines<T: BufRead + Sized>(reader: T, re: Regex) {
     for line_ in reader.lines() {
         let line = line_.unwrap();
         match re.find(&line) {
