e60c0c201dd223ea9ad26492e053dce568eb13ba
Muhammad Falak R Wani <falakreyaz@gmail.com>
2020-01-18 18:38:35 +0530

    grep-lite: add opt to read from files
    Signed-off-by: Muhammad Falak R Wani <falakreyaz@gmail.com>

--- grep-lite/src/main.rs
+++ grep-lite/src/main.rs
@@ -1,21 +1,38 @@
 use clap::{App, Arg};
 use regex::Regex;
 
+use std::fs::File;
+use std::io::prelude::*;
+use std::io::BufReader;
+
 fn main() {
     let args = App::new("grep-lite")
         .version("0.1")
         .about("searches for patterns")
-        .arg(Arg::with_name("pattern").takes_value(true).required(true))
+        .arg(
+            Arg::with_name("pattern")
+                .help("The pattern to search for")
+                .takes_value(true)
+                .required(true),
+        )
+        .arg(
+            Arg::with_name("input")
+                .help("File to search")
+                .takes_value(true)
+                .required(true),
+        )
         .get_matches();
 
     let pattern = args.value_of("pattern").unwrap();
     let re = Regex::new(pattern).unwrap();
 
-    let quote = "Every face, every shop, bedroom window, public-house, and dark square is a picture feverishly turned--in search of what?
-It is the same with books. What do we seek through millions of pages?";
+    let input = args.value_of("input").unwrap();
+    let f = File::open(input).unwrap();
+    let reader = BufReader::new(f);
 
-    for line in quote.lines() {
-        match re.find(line) {
+    for line_ in reader.lines() {
+        let line = line_.unwrap();
+        match re.find(&line) {
             Some(_) => println!("{}", line),
             None => (),
         }
