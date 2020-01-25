b290526eae6db19d8b16ca30d707acf5cb948f19
Muhammad Falak R Wani <falakreyaz@gmail.com>
2020-01-26 00:01:40 +0530

    cube-sat: return ownership
    Signed-off-by: Muhammad Falak R Wani <falakreyaz@gmail.com>

--- cube-sat/src/main.rs
+++ cube-sat/src/main.rs
@@ -1,22 +1,30 @@
 #![allow(unused_variables)]
 
 #[derive(Debug)]
+struct CubeSat {
+    id: u64,
+}
+
+#[derive(Debug)]
 enum StatusMessage {
     Ok,
 }
 
-fn check_status(sat_id: u64) -> StatusMessage {
-    StatusMessage::Ok
+fn check_status(sat_id: CubeSat) -> CubeSat {
+    println!("{:?}: {:?}", sat_id, StatusMessage::Ok);
+    sat_id
 }
 
 fn main() {
-    let sat_a = 0;
-    let sat_b = 1;
-    let sat_c = 2;
+    let sat_a = CubeSat { id: 0 };
+    let sat_b = CubeSat { id: 1 };
+    let sat_c = CubeSat { id: 2 };
 
-    let a_status = check_status(sat_a);
-    let b_status = check_status(sat_b);
-    let c_status = check_status(sat_c);
-
-    println!("a: {:?}, b: {:?}, c: {:?}", a_status, b_status, c_status);
+    let sat_a = check_status(sat_a);
+    let sat_b = check_status(sat_b);
+    let sat_c = check_status(sat_c);
+    // random stuff
+    let sat_a = check_status(sat_a);
+    let sat_b = check_status(sat_b);
+    let sat_c = check_status(sat_c);
 }
