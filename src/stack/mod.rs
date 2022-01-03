2629bd1de7c9850e697fae76ef8ff01832eb58c1
Muhammad Falak R Wani <falakreyaz@gmail.com>
2022-01-03 13:08:55 +0530

    stack: implement a generic stack
    Signed-off-by: Muhammad Falak R Wani <falakreyaz@gmail.com>

---
+++ src/stack/mod.rs
@@ -0,0 +1,49 @@
+#![allow(dead_code)]
+
+pub struct Stack<T> {
+    base: Vec<T>,
+}
+
+/// Stack Public Interface
+impl<T> Stack<T> {
+    pub fn new() -> Self {
+        Self {
+            base: Default::default(),
+        }
+    }
+    pub fn with_capacity(n: usize) -> Self {
+        Self {
+            base: Vec::with_capacity(n),
+        }
+    }
+
+    pub fn push(&mut self, value: T) {
+        self.base.push(value)
+    }
+
+    pub fn pop(&mut self) -> Option<T> {
+        self.base.pop()
+    }
+
+    pub fn peek(&self) -> Option<&T> {
+        self.base.last()
+    }
+
+    pub fn len(&self) -> usize {
+        self.base.len()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn iter_stk_test() {
+        let mut stk = Stack::with_capacity(8);
+        for i in 1..9 {
+            stk.push(i);
+        }
+        assert_eq!(stk.len(), 8);
+    }
+}
