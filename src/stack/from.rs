943cbda0c69ab51bb9051532662e886065e3fd07
Muhammad Falak R Wani <falakreyaz@gmail.com>
2022-06-01 16:58:28 +0530

    stack: add a impl From<&[T]> & From<[T;N]>
    Signed-off-by: Muhammad Falak R Wani <falakreyaz@gmail.com>

--- src/stack/from.rs
+++ src/stack/from.rs
@@ -8,9 +8,27 @@ impl<T> FromIterator<T> for Stack<T> {
     }
 }
 
+impl<T> From<&[T]> for Stack<T>
+where
+    T: Copy,
+{
+    fn from(sl: &[T]) -> Self {
+        Stack::from_iter(sl.into_iter().map(|v| *v))
+    }
+}
+
+impl<T, const N: usize> From<[T; N]> for Stack<T>
+where
+    T: Copy,
+{
+    fn from(arr: [T; N]) -> Self {
+        Self::from_iter(arr)
+    }
+}
+
 impl<T> From<Vec<T>> for Stack<T> {
     fn from(v: Vec<T>) -> Self {
-        Stack { base: v }
+        Self { base: v }
     }
 }
 
@@ -28,5 +46,18 @@ mod tests {
     fn into_stk_test() {
         let mut stk: Stack<i32> = vec![1, 2, 3, 4].into();
         assert_eq!(stk.pop(), Some(4));
+    }
+    #[test]
+    fn from_array_test() {
+        let arr: [i32; 4] = [1, 2, 3, 4];
+        let mut stk: Stack<_> = arr.into();
+        assert_eq!(stk.pop(), Some(4));
+    }
+    #[test]
+    fn from_slice_test() {
+        let v = vec![1, 2, 3, 4, 5];
+        let sl = v.as_slice();
+        let mut stk: Stack<_> = sl.into();
+        assert_eq!(Some(5), stk.pop());
     }
 }
